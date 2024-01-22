92e2f605b5894a735d1c5aafb6bdac1b0383f1f9
Oriol Riera Lizcano <152235615+Uriera4@users.noreply.github.com>
2024-01-22 12:03:31 +0100

    "S2.01.Correccions"

--- S2.01.EstructuraDeDadesMySQL/n1exercici1/ScriptOptica.sql
+++ S2.01.EstructuraDeDadesMySQL/n1exercici1/ScriptOptica.sql
@@ -1,24 +1,37 @@
 DROP DATABASE IF EXISTS Optica;
 CREATE DATABASE Optica;
 USE Optica;
+CREATE TABLE adreces (
+	IDAdreça INT NOT NULL AUTO_INCREMENT,
+    Carrer VARCHAR(30) NOT NULL,
+    Numero INT NOT NULL,
+    Pis VARCHAR(5),
+    Porta INT,
+    Ciutat VARCHAR(30) NOT NULL,
+    CodiPostal INT NOT NULL,
+    Pais VARCHAR(30) NOT NULL,
+	PRIMARY KEY (IDAdreça)
+);   
 CREATE TABLE proveidors (
 	IDProveidor INT NOT NULL AUTO_INCREMENT,
 	Nom VARCHAR(60) NOT NULL,
-    Adreça VARCHAR(100) NOT NULL,
+    IDAdreça INT NOT NULL,
     Telefon INT NOT NULL,
     Fax INT NOT NULL,
     NIF VARCHAR(9) NOT NULL,
-	PRIMARY KEY (IDProveidor)
+	PRIMARY KEY (IDProveidor),
+    FOREIGN KEY (IDAdreça) REFERENCES adreces(IDAdreça)
    );
 CREATE TABLE clients (
 	IDClient INT NOT NULL AUTO_INCREMENT,
     Nom VARCHAR(60) NOT NULL,
-    AdreçaPostal VARCHAR(100) NOT NULL,
+    IDAdreça INT NOT NULL,
     Telefon INT NOT NULL,
     CorreuElectronic VARCHAR(60) NOT NULL,
     DataRegistre DATE NOT NULL,
     RecomenatPer VARCHAR(60),
-    PRIMARY KEY (IDClient)
+    PRIMARY KEY (IDClient),
+    FOREIGN KEY (IDAdreça) REFERENCES adreces(IDAdreça)
 );
 CREATE TABLE ulleres (
 	IDUlleres INT NOT NULL AUTO_INCREMENT,
@@ -44,18 +57,27 @@ CREATE TABLE ventes (
     FOREIGN KEY (IDUlleres) REFERENCES ulleres(IDUlleres),
 	FOREIGN KEY (IDClient) REFERENCES clients(IDClient)
 );
-
+/* iniciem adreces */
+INSERT INTO adreces (Carrer, Numero, Ciutat, CodiPostal, Pais) VALUES ("C/ de Goya", 44, "Salamanca", 28001, "España");
+INSERT INTO adreces (Carrer, Numero, Ciutat, CodiPostal, Pais) VALUES ("C/ Angel Guimera", 46, "Palma de Mallorca", 07004, "España");
+INSERT INTO adreces (Carrer, Numero, Ciutat, CodiPostal, Pais) VALUES ("C/ Pallars", 73, "Barcelona", 08018, "España");
+INSERT INTO adreces (Carrer, Numero, Ciutat, CodiPostal, Pais) VALUES ("C/ del Disfrute", 777, "CiudadIncreible", 44444, "España");
+INSERT INTO adreces (Carrer, Numero, Pis, Porta, Ciutat, CodiPostal, Pais) VALUES ("C/ Marina", 200, 2, 1, "Barcelona", 08013, "España");
+INSERT INTO adreces (Carrer, Numero, Pis, Porta, Ciutat, CodiPostal, Pais) VALUES ("C/ Progreso", 32, 1, 1, "Valencia", 12547, "España");
+INSERT INTO adreces (Carrer, Numero, Pis, Porta, Ciutat, CodiPostal, Pais) VALUES ("C/ Esparrago", 101, 7, 3, "Bilbao", 44201, "España");
+INSERT INTO adreces (Carrer, Numero, Pis, Porta, Ciutat, CodiPostal, Pais) VALUES ("C/ Sense Nom", 1, "baix", 2, "Zarautz", 33331, "España");
+INSERT INTO adreces (Carrer, Numero, Pis, Porta, Ciutat, CodiPostal, Pais) VALUES ("C/ Amb Nom", 999, 4, 2, "Salamanca", 14200, "España");
 /* iniciem proveidors */
-INSERT INTO proveidors (Nom, Adreça, Telefon, Fax, NIF) VALUES ("MULTIOPTICAS", "C/ de Goya, 44, Salamanca, Madrid, 28001, España", 915753430, 915753430, "F28465193");
-INSERT INTO proveidors (Nom, Adreça, Telefon, Fax, NIF) VALUES ("GENERAL OPTICA", "C/ Angel Guimera 46, Palma de Mallorca - Islas Baleares, 07004, España", 971582799, 971582580, "B07887250");
-INSERT INTO proveidors (Nom, Adreça, Telefon, Fax, NIF) VALUES ("PROSUN", "C/ Pallars, 73 - BAJO, Barcelona (Ciudad) - Barcelona, 08018, España", 934392051, 934392051, "A08829483");
-INSERT INTO proveidors (Nom, Adreça, Telefon, Fax, NIF) VALUES ("MI OPTICA PREFERIDA", "C/ del Disfrute, 777, Maravilla, CiudadIncreible, 44444, España", 999777666, 999111000, "O12345678");
+INSERT INTO proveidors (Nom, IDAdreça, Telefon, Fax, NIF) VALUES ("MULTIOPTICAS", 1, 915753430, 915753430, "F28465193");
+INSERT INTO proveidors (Nom, IDAdreça, Telefon, Fax, NIF) VALUES ("GENERAL OPTICA", 2, 971582799, 971582580, "B07887250");
+INSERT INTO proveidors (Nom, IDAdreça, Telefon, Fax, NIF) VALUES ("PROSUN", 3, 934392051, 934392051, "A08829483");
+INSERT INTO proveidors (Nom, IDAdreça, Telefon, Fax, NIF) VALUES ("MI OPTICA PREFERIDA", 4, 999777666, 999111000, "O12345678");
 /* iniciem clients */
-INSERT INTO clients (Nom, AdreçaPostal, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Oriol", "C/ Marina, 200, 2n/1a, Barcelona, 08013, España", 648849287, "oriolriera@hotmail.com", '2007-01-15', NULL);
-INSERT INTO clients (Nom, AdreçaPostal, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Maria", "C/ Progreso, 32, 1o/1a, Valencia, 12547, España", 653328751, "mariasoyyo@hotmail.com", '2020-05-23', "Oriol");
-INSERT INTO clients (Nom, AdreçaPostal, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Arnau", "C/ Esparrago, 101, 7o/3a, Bilbao, 44201, España", 674420311, "eselcorreoreal@hotmail.com", '2007-07-07', "Oriol");
-INSERT INTO clients (Nom, AdreçaPostal, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Elvira", "C/ Sense Nom, 1, baix/2a, Zarautz, 33331, España", 698765432, "elviratimo@hotmail.com", '2023-02-01', NULL);
-INSERT INTO clients (Nom, AdreçaPostal, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Didac", "C/ Amb Nom, 999, 4t/2a, Salamanca, 14200, España", 666111222, "didactic@hotmail.com", '2024-01-10', "Maria");
+INSERT INTO clients (Nom, IDAdreça, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Oriol", 5, 648849287, "oriolriera@hotmail.com", '2007-01-15', NULL);
+INSERT INTO clients (Nom, IDAdreça, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Maria", 6, 653328751, "mariasoyyo@hotmail.com", '2020-05-23', "Oriol");
+INSERT INTO clients (Nom, IDAdreça, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Arnau", 7, 674420311, "eselcorreoreal@hotmail.com", '2007-07-07', "Oriol");
+INSERT INTO clients (Nom, IDAdreça, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Elvira", 8, 698765432, "elviratimo@hotmail.com", '2023-02-01', NULL);
+INSERT INTO clients (Nom, IDAdreça, Telefon, CorreuElectronic, DataRegistre, RecomenatPer) VALUES ("Didac", 9, 666111222, "didactic@hotmail.com", '2024-01-10', "Maria");
 /* iniciem ulleres */
 INSERT INTO ulleres (Marca, TipusMontura, ColorMontura, ColorVidreEsquerra, ColorVidreDreta, Preu, IDProveidor) VALUES ("Ray-Ban", "metalica", "negra", "translucid", "translucid", 110, 1);
 INSERT INTO ulleres (Marca, TipusMontura, ColorMontura, ColorVidreEsquerra, ColorVidreDreta, Preu, IDProveidor) VALUES ("Ray-Ban", "pasta", "vermella", "verd", "verd", 85, 1);
